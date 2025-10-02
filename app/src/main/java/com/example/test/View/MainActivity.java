209606005285997b4a130f5caf835998d846a054
Anyeel <angelmillanaragon@hotmail.com>
2025-10-02 09:10:37 +0200

    Primer commit - subiendo Calculadora

---
+++ app/src/main/java/com/example/test/View/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.test.View;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnInicio;
+        TextView title;
+
+        btnInicio = (Button)findViewById(R.id.btnInicio);
+        title = (TextView)findViewById(R.id.txtCalculadora);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title.setText("This is a test");
+            }
+        });
+    }
+}
